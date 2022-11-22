568b8e943b2da9576ede9a5d491dee02577f9b07
Natã Martins <nata@Natas-MacBook-Air.local>
2022-11-22 15:22:41 -0300

    new update ex02

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Exercico01 from './components/exercicio01';
+import Exercico02 from './components/exercicio02';
 
 const App = () => {
   return (
     <div>
       <Exercico01 />
+      <Exercico02 />
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './components/exercicio01/style.css';
+import './components/exercicio02/style.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
