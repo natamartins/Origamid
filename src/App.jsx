180b2aa3caea390368d670903920b98d0fd68a78
Natã Martins <nata@Natas-MacBook-Air.local>
2022-11-25 13:48:00 -0300

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import { Index } from './components/exercicio03/Index';
 // import Index04 from './components/exercico04/Index';
 // import Index05 from './components/exercicio05/Index';
-import Index06 from './components/exercicio06/Index';
+import Index08 from './components/exercicio08/Index';
 // import Home from './pages/home';
 
 const App = () => {
@@ -13,7 +13,9 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index06 />} exact />
+          <Route path="/" element={<Index08 />} exact />
+          {/* <Route path="/" element={<Index07 />} exact /> */}
+          {/* <Route path="/" element={<Index06 />} exact /> */}
           {/* <Route path="/" element={<Index05 />} exact /> */}
           {/* <Route path="/" element={<Index04 />} exact /> */}
           {/* <Route path="/" element={<Home />} exact /> */}
